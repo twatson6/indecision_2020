b900cec5d7cef383d522af8593d68ef71f94d4a9
twatson6 <twatson6@csub.edu>
2020-02-12 00:54:44 -0800

    First commit, hopefully works fine :D

---
+++ src/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿//Written by Terry Watson 2.14.2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Increments scene to Game scene TW
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	//Sends Application Quit, also prints Pressed Quit for presses in the editor TW
+	public void QuitGame()
+	{
+		Debug.Log("Pressed Quit!");
+		Application.Quit();
+	}
+}
