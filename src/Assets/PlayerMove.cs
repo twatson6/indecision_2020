ddf8aaa471084410466936d54aa32948d1c77217
twatson6 <twatson6@csub.edu>
2020-02-29 19:44:41 -0800

    Added in first grass placeholder stage, added player, movement, and shooting.

---
+++ src/Assets/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿//made by Terry for the Player Movement 02/29/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public float playerSpeed = 3f;
+	private float lookAngle;
+	
+	public Camera camera;
+	public Rigidbody2D rigidBody;
+	
+	private Vector2 mouse;
+	private Vector2 lookHere;
+	public Vector2 movement;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        
+		mouse = camera.ScreenToWorldPoint(Input.mousePosition);
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+		
+    }
+	
+	void FixedUpdate()
+	{
+		rigidBody.MovePosition(rigidBody.position + playerSpeed * movement * Time.fixedDeltaTime);
+		
+		lookHere = mouse - rigidBody.position;
+		lookAngle = Mathf.Atan2(lookHere.y, lookHere.x) * Mathf.Rad2Deg;
+		rigidBody.rotation = lookAngle;
+	}
+}
---
+++ src/Assets/Shoot.cs
@@ -0,0 +1,33 @@
+﻿// made by Terry for shooting 2/29/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+	public Transform firePoint;
+	
+	public GameObject bulletPrefab;
+	
+	public float fireRate = 1f;
+	
+	public float canFire = 1f;
+	public float bulletForce = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1")&&Time.time > canFire)
+		{
+			Fire();
+			canFire = Time.time + fireRate;
+		}
+    }
+	
+	void Fire()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
+		bulletBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+	}
+}
