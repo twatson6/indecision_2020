715e1f97593144b32454c212312d559b457630d4
Terry <terry.watson1818@gmail.com>
2020-03-04 14:25:49 -0800

    Made camera follow player

---
+++ src/Assets/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿//Made by Terry for camera following on 03/04/2020
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+	public Transform Player;
+	public float smoother = 0.8f;
+	
+	private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goal = Player.position;
+		goal.z = transform.position.z;
+		
+		transform.position = Vector3.SmoothDamp(transform.position, goal, ref velocity, smoother);
+    }
+}
