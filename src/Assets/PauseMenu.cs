937552ed6f88079fc95123c15a688923701ac417
Terry <terry.watson1818@gmail.com>
2020-03-04 17:28:20 -0800

    Added in the pause menu boys.

---
+++ src/Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public static bool isPaused = false;
+	
+	public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(isPaused)
+			{
+				resume();
+			}
+			else
+			{
+				pause();
+			}
+			
+		}
+    }
+	
+	public void resume ()
+	{
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1f;
+		isPaused = false;
+	}
+	
+	void pause ()
+	{
+		pauseMenu.SetActive(true);
+		Time.timeScale = 0f;
+		isPaused = true;
+	}
+	
+	public void menu ()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	
+	public void quit ()
+	{
+		Application.Quit();
+	}
+}
