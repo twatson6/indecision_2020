d62472dd7d300bbbe12db9ddcbc8082847be3ef4
davidxmontes <davidxmontes@gmail.com>
2020-03-14 16:05:38 -0700

    Fixed an issue we were experiencing with the zombie's speed slowing down as it came closer to the player

--- src/Assets/ZombieMovement.cs
+++ src/Assets/ZombieMovement.cs
@@ -6,9 +6,10 @@ public class ZombieMovement : MonoBehaviour
 {
     public Transform Player;
     public float moveSpeed = 5f;
+    public float defaultSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
-
+    
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -18,18 +19,32 @@ public class ZombieMovement : MonoBehaviour
     void Update()
     {
         Vector3 direction = Player.position - transform.position;
+        setMoveSpeed(direction);
+       
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
         direction.Normalize();
         movement = direction;
     }
 
-    void moveCharacter(Vector2 direction)
-    {
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.fixedDeltaTime));
-    }
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         moveCharacter(movement);
     }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void setMoveSpeed(Vector3 dir)
+    {
+        if (System.Math.Abs(dir.y) < 2.5 && System.Math.Abs(dir.x) < 2.5)
+        {
+            moveSpeed = defaultSpeed * 1.5f;
+        }
+        else
+            moveSpeed = defaultSpeed;
+    }
+
 }
