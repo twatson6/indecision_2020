b4174f122de8768fc2d8b323f96f1fef97a37a81
Tony Cervantes <tony@xlrcrashs-mbp.dyn.csub.edu>
2020-04-02 14:08:39 -0700

    Testing branches

--- src/Assets/Bullet.cs
+++ src/Assets/Bullet.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	public GameObject Explosion;
+
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Destroy(gameObject);
+		GameObject explode = Instantiate(Explosion, transform.position, Quaternion.identity);
+		Destroy(explode, 2f);
+        Destroy(gameObject);
 	}
 }
