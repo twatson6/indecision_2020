2940e19ce0f50a4f57497c7dad4189a6ad12473e
Xlrcrash1 <60253517+Xlrcrash1@users.noreply.github.com>
2020-04-18 23:08:28 -0700

    Currently working on more soand added a temp theme to the main menu

--- src/Assets/MainMenu.cs
+++ src/Assets/MainMenu.cs
@@ -10,11 +10,13 @@ public class MainMenu : MonoBehaviour
 	public void PlayGame()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		
 	}
 	//Sends Application Quit, also prints Pressed Quit for presses in the editor TW
 	public void QuitGame()
 	{
 		Debug.Log("Pressed Quit!");
 		Application.Quit();
+        FindObjectOfType<AudioManager>().Play("Main_Menu");
 	}
 }
