f8dcfa8bcd29a47a7867313b0664fe6d7689ab11
Terry <terry.watson1818@gmail.com>
2020-04-19 18:37:33 -0700

    Added scorekeeper and increments score by one on zombie death

---
+++ src/Assets/scoreKeeper.cs
@@ -0,0 +1,15 @@
+﻿//written by Terry 04/19/2020 to keep Score
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class scoreKeeper : MonoBehaviour
+{
+	static public int score = 0;
+	public TextMeshProUGUI scoreComponent;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreComponent.text = "SCORE: " + score.ToString();
+    }
+}
