9b81c9bc2f56b293ebe35a273631fc3b0983ee60
Terry <terry.watson1818@gmail.com>
2020-04-20 17:34:06 -0700

    Added volume control in main menu

---
+++ src/Assets/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+	public AudioMixer audioMix;
+	void Start()
+	{
+		audioMix.SetFloat("volume", -20f);
+	}
+	public void SetVolume (float volume)
+	{
+		audioMix.SetFloat("volume", volume);
+	}
+}
