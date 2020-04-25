6b332edacf236cfa21cacec99da43643295a6346
davidxmontes <davidxmontes@gmail.com>
2020-04-24 23:07:02 -0700

    Manually added Terry's score, credits, and volume slider changes

---
+++ src/Assets/SettingsMenu.cs
@@ -0,0 +1,17 @@
+﻿//Written by Terry 04/20/20 to control the volume slider in settings
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+	public AudioMixer audioMix;
+	void Start()
+	{
+		audioMix.SetFloat("volume", -20f);
+	}
+	public void SetVolume (float volume)
+	{
+		audioMix.SetFloat("volume", volume);
+	}
+}
